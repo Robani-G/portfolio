36f6145a75e80e5e85e0106e35d2d67e7bf06e34
robani-g <robanigirma2021@gmail.com>
2023-10-18 14:39:24 +0300

    button fixed

--- src/app/componets/main/Intro.tsx
+++ src/app/componets/main/Intro.tsx
@@ -48,23 +48,24 @@ function Intro() {
       <div className=" item-center mt-4 flex flex-col justify-center gap-2 px-4 sm:flex-row   ">
         <a
           href="/portfolio.pdf"
-          className="flex w-full items-center justify-center rounded-lg bg-gray-600 hover:bg-gray-900 text-white dark:bg-gray-900  p-2 text-black sm:w-1/3"
+          className="flex w-full items-center justify-center rounded-lg bg-gray-600 hover:bg-gray-900 text-white dark:bg-gray-900  p-2 text-black sm:w-1/4"
         >
           Download Cv <ArrowDownCircleIcon className="h-7 w-5" />
         </a>
         <Link
           href="#Contact"
-          className="flex w-full items-center justify-center rounded-lg bg-gray-900  dark:bg-white hover:bg-gray-600 p-2 text-white dark:text-black sm:w-1/3"
+          className="flex w-full items-center justify-center rounded-lg bg-gray-900  dark:bg-white hover:bg-gray-600 p-2 text-white dark:text-black sm:w-1/4"
         >
           Contact me here
           <ArrowLongRightIcon className="h-5 w-7" />
         </Link>
-      </div>
-      <div className=" item-center mt-4 flex flex justify-center gap-2 px-4    ">
+        <div className=" item-center mt-4 flex flex justify-center gap-2 px-4    ">
         
       <a href="https://github.com/Robani-G"><AiFillGithub className="h-9 w-9 dark:text-white"/></a>
       <a href="#"><AiFillLinkedin className="h-9 w-9 dark:text-white"/></a>
       </div>
+      </div>
+      
     </>
   );
 }
